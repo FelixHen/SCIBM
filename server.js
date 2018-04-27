3bcda759f81f9d63fd9de20ae329ea82ce792fbf
Marija <sharpeima@gmail.com>
2018-04-27 14:18:14 +0200

    tone test

--- server.js
+++ server.js
@@ -189,6 +189,73 @@ io.on('connection', function(socket){
 
 });
 
+
+
+function createToneRequest (request) {
+  let toneChatRequest;
+
+  if (request.texts) {
+    toneChatRequest = {utterances: []};
+
+    for (let i in request.texts) {
+      let utterance = {text: request.texts[i]};
+      toneChatRequest.utterances.push(utterance);
+    }
+  }
+
+  return toneChatRequest;
+}
+
+function happyOrUnhappy (response) {
+  const happyTones = ['satisfied', 'excited', 'polite', 'sympathetic'];
+  const unhappyTones = ['sad', 'frustrated', 'impolite'];
+
+  let happyValue = 0;
+  let unhappyValue = 0;
+
+  for (let i in response.utterances_tone) {
+    let utteranceTones = response.utterances_tone[i].tones;
+    for (let j in utteranceTones) {
+      if (happyTones.includes(utteranceTones[j].tone_id)) {
+        happyValue = happyValue + utteranceTones[j].score;
+      }
+      if (unhappyTones.includes(utteranceTones[j].tone_id)) {
+        unhappyValue = unhappyValue + utteranceTones[j].score;
+      }
+    }
+  }
+  if (happyValue >= unhappyValue) {
+    return 'happy';
+  }
+  else {
+    return 'unhappy';
+  }
+}
+
+/* Example 
+{
+  "texts": ["I do not like what I see", "I like very much what you have said."]
+}
+*/
+app.post('/tone', (req, res, next) => {
+  let toneRequest = createToneRequest(req.body);
+
+  if (toneRequest) {
+    toneAnalyzer.tone_chat(toneRequest, (err, response) => {
+      if (err) {
+        return next(err);
+      }
+      let answer = {mood: happyOrUnhappy(response)};
+	  alert(answer);
+      return res.json(answer);
+    });
+  }
+  else {
+    return res.status(400).send({error: 'Invalid Input'});
+  }
+});
+
+
 /*
 listen on Port XXXX
 */	
