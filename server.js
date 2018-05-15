e573e51c62a16d68d75c722b358c8efdc573075f
Felix Hennig <felix_maximilian.hennig@student.reutlingen-university.de>
2018-05-15 16:19:24 +0200

    cert files

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ var options = {
 };
 var server = https.createServer(options, app);
 
-let io = require('socket.io');
+let io = require('socket.io')(server);
 let port = process.env.PORT || process.env.VCAP_APP_PORT || 3000;
 var bodyParser = require('body-parser');
 var mysql = require('mysql'); 
